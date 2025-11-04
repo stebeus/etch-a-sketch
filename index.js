282d1e74d4d02d4cb89bfd0db7ebb1d5d20a5284
stebeus <stebeus@mailfence.com>
2025-11-04 14:04:31 -0300

    Create and clone divPixel to the DOM

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 "use strict";
+
+const container = document.querySelector(".container");
+const divPixel = document.createElement("div");
+divPixel.classList.add("container__div-pixel");
+
+for (let i = 0; i < 16; i++) {
+  container.appendChild(divPixel.cloneNode());
+}
