da27e72915414946b7a79015307441d08b9db498
stebeus <stebeus@mailfence.com>
2025-11-05 15:02:05 -0300

    Add clear canvas button and query select all buttons

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 
 const canvas = document.querySelector(".canvas");
 const divPixel = document.createElement("div");
+
+const btnSetGrid = document.querySelector("[data-action='set-grid']");
+const btnResetGrid = document.querySelector("[data-action='reset-grid']");
+const btnClearCanvas = document.querySelector("[data-action='clear-canvas']");
+
 divPixel.classList.add("canvas__pixel");
 
 let gridNum = 16 ** 2;
